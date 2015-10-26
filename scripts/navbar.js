1690abb66a7ce6df42dbd2868f27b722453a39db
AoiBurizado <aoi_burizado@hotmail.com>
2015-10-26 13:31:13 +0100

    navbar still under construction (need scaling), css changed to support scaling and positioning og multimedia and pictures. Added video example to template

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -4,6 +4,7 @@ var docked = true;
 window.onscroll = function() {
 	if (docked && document.body.scrollTop >= 240) {
 		navbar.style.position = "fixed";
+		navbar.style.left = "25%";
 		navbar.style.top = "0px";
 		docked = false;
 	} else if (!docked && document.body.scrollTop < 240) {
