84213d884f457d6a0330a982be9f2eb032855029
AoiBurizado <aoi_burizado@hotmail.com>
2015-10-26 14:15:05 +0100

    fixed navbar on scroll

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -4,8 +4,9 @@ var docked = true;
 window.onscroll = function() {
 	if (docked && document.body.scrollTop >= 240) {
 		navbar.style.position = "fixed";
-		navbar.style.left = "25%";
-		navbar.style.top = "0px";
+		navbar.style.right = "0";
+		navbar.style.left = "0";
+		navbar.style.top = "0";
 		docked = false;
 	} else if (!docked && document.body.scrollTop < 240) {
 		navbar.style.position = "static";
