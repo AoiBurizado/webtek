d1d283c82c1109ea924bb045c0efd530f3c5ff96
krislerv <kristoffer-lervik@hotmail.com>
2015-10-28 14:20:32 +0100

    updated konami code

--- scripts/konamicode.js
+++ scripts/konamicode.js
@@ -1,6 +1,9 @@
 var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 var konamiCounter = 0;
-var interval;
+var people = ["dag", "iselin", "kristoffer", "marius", "my"];
+var currentPerson = 0;
+var moveInterval;
+var createPersonInterval;
 
 window.onkeydown = function(event) {
 	if (event.keyCode == code[konamiCounter]) {
@@ -14,23 +17,32 @@ window.onkeydown = function(event) {
 }
 
 function fun() {
+	createPersonInterval = window.setInterval(function() {createPerson(people[currentPerson]);}, 200);
+	moveInterval = window.setInterval(move, 30);
+}
+
+function createPerson(name) {
 	var img = document.createElement("img");
-	img.src = "../images/animegirl.gif";
-	img.id = "asdf";
+	img.src = "../images/running_" + people[i] + ".gif";
+	console.log(img.src);
+	img.className = "groupmembers";
 	img.style.position = "absolute";
 	document.body.appendChild(img);
-	interval = window.setInterval(move, 30);
+	currentPerson++;
+	if (currentPerson >= people.length) {
+		clearInterval(createPersonInterval);
+	}
 }
 
 function move() {
-	var left = document.getElementById("asdf").style.left;
+	var left = document.getElementById("groupmembers").style.left;
 	left = left.substr(0, left.length - 2);
 	if (Number(left) > window.innerWidth) {
-		document.body.removeChild(document.getElementById("asdf"));
-		clearInterval(interval);
+		document.body.removeChild(document.getElementById("groupmembers"));
+		clearInterval(moveInterval);
 		konamiCounter = 0;
 		return;
 	}
-	document.getElementById("asdf").style.left = (Number(left) + 5) + "px";
-	document.getElementById("asdf").style.top = document.body.scrollTop + window.innerHeight - 128 + "px";
+	document.getElementById("groupmembers").style.left = (Number(left) + 5) + "px";
+	document.getElementById("groupmembers").style.top = document.body.scrollTop + window.innerHeight - 128 + "px";
 }
