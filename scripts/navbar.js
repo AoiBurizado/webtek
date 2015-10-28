07088afec8dc359c6593f7974fcfc0286da3e03b
AoiBurizado <aoi_burizado@hotmail.com>
2015-10-28 12:28:13 +0100

    Added review folder and updates navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -4,12 +4,16 @@ var docked = true;
 window.onscroll = function() {
 	if (docked && document.body.scrollTop >= 240) {
 		navbar.style.position = "fixed";
+		navbar.style.width="750px";
 		navbar.style.right = "0";
-		navbar.style.left = "0";
+		navbar.style.left="0";
 		navbar.style.top = "0";
+		navbar.style.borderRadius = "0em 0em 0.3em 0.3em";
 		docked = false;
 	} else if (!docked && document.body.scrollTop < 240) {
 		navbar.style.position = "static";
+		navbar.style.width="700px";
+		navbar.style.borderRadius = "0.3em";
 		docked = true;
 	}
 }
