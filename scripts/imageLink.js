579849e47a742babe3b2973e8fb5ee196536af04
krislerv <kristoffer-lervik@hotmail.com>
2015-11-11 13:09:04 +0100

    fix

--- scripts/imageLink.js
+++ scripts/imageLink.js
@@ -2,7 +2,7 @@ var images = document.getElementsByTagName("img");
 
 for(var i = 0; i < images.length; i++) {
     var image = images[i];
-    if (image.class == "thumbnailPicture") {
+    if (image.className == "thumbnailPicture") {
     	image.onclick = function(event) {
         	window.location.href = this.id + '.html';
     }
