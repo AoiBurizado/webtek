d9409cee2e606060bc46bdda1f7ed977d8c66536
Marius Enerly <marius.enerly@gmail.com>
2015-11-11 13:03:44 +0100

    fixed my shit

--- scripts/imageLink.js
+++ scripts/imageLink.js
@@ -1,9 +1,8 @@
-//burde ha funnet bilder av klassen "thumbnailPicture" i stedet for alle bilder. 
 var images = document.getElementsByTagName("img");
 
 for(var i = 0; i < images.length; i++) {
     var image = images[i];
-    if (!(image.id == "headerImage")) {
+    if (image.class == "thumbnailPicture") {
     	image.onclick = function(event) {
         	window.location.href = this.id + '.html';
     }
