87892ce53736916f9a75165037052459ce0e36f4
Marius Enerly <marius.enerly@gmail.com>
2015-11-11 12:44:44 +0100

    fixed your shit, mofo

---
+++ scripts/imageLink.js
@@ -0,0 +1,11 @@
+
+var images = document.getElementsByTagName("img");
+
+for(var i = 0; i < images.length; i++) {
+    var image = images[i];
+    if (!(image.id == "headerImage")) {
+    	image.onclick = function(event) {
+        	window.location.href = this.id + '.html';
+    }
+    };
+}
